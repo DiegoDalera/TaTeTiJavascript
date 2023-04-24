a1c2820931e3b16afb112ce977fc2b478ebd40f6
diego <dmdalera@gmail.com>
2023-04-24 18:10:47 -0300

    juego terminado V1

--- logica.js
+++ logica.js
@@ -1,25 +1,72 @@
-let textPlayer = document.getElementById('player_text');
-let resetBtn = document.getElementById('reset_btn');
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('casilla'))
 
-let casillas = Array.from(document.getElementsByClassName('casilla'));
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
 
-const text_x = "x";
-const texto_y = "y";
-let jugadrActual=text_x;
-
-let espacios = Array(9).fill(null);
-
-
-
-const comienzoJuego = () => {
-    casillas.forEach(casilla => casilla.addEventListener('click',casillaClick ))
-
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
-function casillaClick(e){
-    console.log(e.target);
+function boxClicked(e) {
+    const id = e.target.id
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( casilla => boxes[casilla].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
 }
 
-comienzoJuego();
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false
+}
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach( casilla => {
+        casilla.innerText = ''
+        casilla.style.backgroundColor=''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_TEXT
+}
+
+startGame()
