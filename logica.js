acec6213238b4e771c3fcb4f9b83f9984992d3d7
diego <dmdalera@gmail.com>
2023-07-04 16:18:24 -0300

    add win blocks

--- logica.js
+++ logica.js
@@ -2,18 +2,19 @@ let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
 let boxes = Array.from(document.getElementsByClassName('casilla'))
 
-let ganador= getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+//let ganador= getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
 const jugador_0= "O"
 const jugador_X = "X"
 let jugadorActual = jugador_X
+console.log(boxes)
 let spaces = Array(9).fill(null)
 
 const startGame = () => {
-    boxes.forEach(box => box.addEventListener('click', boxClicked))
+    boxes.forEach(box => box.addEventListener('click', clickBox))
 }
 
-function boxClicked(e) {
+function clickBox(e) {
     const id = e.target.id
 
     if(!spaces[id]){
@@ -23,8 +24,13 @@ function boxClicked(e) {
         if(playerHasWon() !==false){
             playerText.innerHTML = `${jugadorActual} gano la partida!`
             let winning_blocks = playerHasWon()
+            console.log(winning_blocks);
+            winning_blocks.map( casilla => boxes[casilla].style.backgroundColor='red');
 
-            winning_blocks.map( casilla => boxes[casilla].style.backgroundColor=winnerIndicator)
+            for (let i = 0; i < boxes.length; i++) {
+                boxes[i].classList.add('deshabilitado');
+              }
+
             return
         }
 
@@ -32,7 +38,7 @@ function boxClicked(e) {
     }
 }
 
-const winningCombos = [
+const winCombos = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -44,9 +50,11 @@ const winningCombos = [
 ]
 
 function playerHasWon() {
-    for (const condition of winningCombos) {
+    for (const condition of winCombos) {
+        //desestructuro los combos ganadores
         let [a, b, c] = condition
 
+        //verifica si todas las casillas en las posiciones a, b y c están ocupadas por el mismo jugador
         if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
             return [a,b,c]
         }
@@ -57,6 +65,11 @@ function playerHasWon() {
 restartBtn.addEventListener('click', restart)
 
 function restart() {
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.remove('deshabilitado')
+      }
+
     spaces.fill(null)
 
     boxes.forEach( casilla => {
